0b1e8a2ae808450fd695f35da9f35b46fe148395
Liam Ma <liam.q.ma@gmail.com>
2018-07-21 16:28:33 +1000

    Add validation and README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Add tags to Load Balancer by Elasticbeanstalk environment name
+
+## Usage
+
+```Javascript
+$ npm i -g aws-add-tags-load-balancer
+$ aws-add-tags-load-balancer --awsKeysFilePath=./credentials.yaml --environmentName=pickle-prod --tag.key1=value1 --tag.key2=value2
+```
--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-console.log(JSON.stringify(process.argv));
+#!/usr/bin/env node
+var yaml = require('js-yaml');
+var fs = require('fs');
+var argv = require('yargs').argv;
+var AWS = require('aws-sdk');
+var invariant = require('invariant');
+var _ = require('lodash');
+
+var PROJECT_NAME = 'ADD-TAGS';
+
+function getAwsCredentials(keyFile) {
+    return yaml.safeLoad(fs.readFileSync(keyFile, 'utf8'));
+}
+
+function main() {
+    invariant(argv.awsKeysFilePath, 'awsKeysFilePath is required');
+    invariant(argv.environmentName, 'environmentName is required');
+    invariant(typeof argv.tag === 'object', 'tag is required');
+    
+    var awsCredentials = getAwsCredentials(argv.awsKeysFilePath);
+    var environmentName = argv.environmentName;
+    var tags = [];
+    _.forOwn(argv.tag, function(value, key) {
+        tags.push({
+            Key: key,
+            Value: value,
+        });
+    });
+
+    var elasticbeanstalk = new AWS.ElasticBeanstalk({
+        accessKeyId: awsCredentials.access_key_id,
+        secretAccessKey: awsCredentials.secret_access_key,
+        sessionToken: awsCredentials.session_token || null,
+        region: awsCredentials.region || 'ap-southeast-2',
+    });
+    
+    var elb = new AWS.ELB({
+        accessKeyId: awsCredentials.access_key_id,
+        secretAccessKey: awsCredentials.secret_access_key,
+        sessionToken: awsCredentials.session_token || null,
+        region: awsCredentials.region || 'ap-southeast-2',
+    });
+    
+    elasticbeanstalk.describeEnvironmentResources({
+        EnvironmentName: environmentName
+    }, function(err, data) {
+        if (err) {
+            console.log(err, err.stack); // an error occurred
+            return;
+        } 
+        
+        var LoadBalancerName = _.get(data, 'EnvironmentResources.LoadBalancers[0].Name');
+        invariant(LoadBalancerName, 'Load Balancer is not found');
+        console.log(PROJECT_NAME + ': Found envrionment. The LoadBalancerName is ' + LoadBalancerName); // successful response
+    
+        elb.addTags({
+            LoadBalancerNames: [
+                LoadBalancerName
+            ], 
+            Tags: tags
+        }, function(err, data) {
+            if (err) {
+                console.log(err, err.stack); // an error occurred
+            } else { 
+                console.log(PROJECT_NAME + ': Done'); // successful response
+            }
+        })
+    });
+}
+
+main();
